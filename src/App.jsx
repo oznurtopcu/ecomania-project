d1df479778caed2ff11ba865feefa2fccb0d03c4
oznurtopcu <topcuoznur121@gmail.com>
2025-02-12 13:08:57 +0300

    start header css

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
       <Header/>
     </>
   )
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,17 +2,27 @@ import { Menu, Search, ShoppingCart } from "lucide-react";
 
 export default function Header() {
     return(
-        <header>
-            <h1>BrandName</h1>
-            <nav>
+        <header className="">
+            <div className="flex justify-between items-center mx-8 my-4 text-3xl text-[#737373]">
+                <img src="https://placehold.co/187x58" alt="logo" />
+                <nav className="hidden">
+                    <a href="#">Home</a>
+                    <a href="#">Product</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Contact</a>
+                </nav>
+                <div className="flex gap-5">
+                    <Search />
+                    <ShoppingCart size={20} />
+                    <Menu size={20} />
+                </div>
+            </div>
+            <nav className="flex flex-col items-center gap-8 mx-8 my-8 text-3xl text-[#737373]">
                 <a href="#">Home</a>
                 <a href="#">Product</a>
                 <a href="#">Pricing</a>
                 <a href="#">Contact</a>
             </nav>
-            <Search />
-            <ShoppingCart size={20} />
-            <Menu size={20} />
 
 
         </header>
