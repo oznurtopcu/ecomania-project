afbfde4ae2ea084db174e48d5e04d41f292c061f
oznurtopcu <topcuoznur121@gmail.com>
2025-02-12 12:27:29 +0300

    create header nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './layout/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Hello World</h1>
+      <Header/>
     </>
   )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,20 @@
+import { Menu, Search, ShoppingCart } from "lucide-react";
+
 export default function Header() {
     return(
         <header>
-            <p>Hello world!</p>
+            <h1>BrandName</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Product</a>
+                <a href="#">Pricing</a>
+                <a href="#">Contact</a>
+            </nav>
+            <Search />
+            <ShoppingCart size={20} />
+            <Menu size={20} />
+
+
         </header>
     );
 }
