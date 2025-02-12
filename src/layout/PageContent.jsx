1e55914b0f96b546c6197d18ba1af895931a502b
oznurtopcu <topcuoznur121@gmail.com>
2025-02-12 10:01:13 +0300

    add comment to pagecontent

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -2,6 +2,8 @@ import Footer from "./Footer";
 import Header from "./Header"
 
 export default function PageContent( {children} ) {
+    //Bu component header ve footer'ın her sayfada aynı şekilde görünmesini sağlıyor. 
+    //Componentleri PageContent ile sarmalıyoruz.
     return(
         <div>
             <Header/>
