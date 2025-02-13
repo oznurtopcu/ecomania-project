8247df6eb87778e92e5575b90cefb425afef3031
oznurtopcu <topcuoznur121@gmail.com>
2025-02-13 16:15:56 +0300

    add Slider component in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,29 +2,10 @@ import ShopCard from "../components/ShopCard";
 import Slider from "../components/Slider";
 
 export default function HomePage() {
-  const sampleSlides = [
-    {
-      image: "", // Replace with the actual URL of your image (or use require('./image.jpg') if local)
-      summerText: "SUMMER 2020",
-      newCollectionText: "NEW COLLECTION",
-      description:
-        "We know how large objects will act, but things on a small scale.",
-      buttonText: "SHOP NOW",
-    },
-    {
-      image: "", // Add more slides as needed
-      summerText: "SUMMER 2021", // Example different content
-      newCollectionText: "Another Collection",
-      description: "Example description for the second slide.",
-      buttonText: "EXPLORE MORE",
-    },
-    // ... more slides
-  ];
-
   return (
-    <div className="container">
-      <div className="slider">
-        <Slider slides={sampleSlides} />
+    <div className="@container">
+      <div className="slider-hero">
+        <Slider />
         {/* <img src="https://placehold.co/414x753" alt="slider" /> */}
       </div>
       <div className="shopping-cards my-15">
