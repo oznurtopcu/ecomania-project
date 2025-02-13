48203fe25e85d6c00a821334fffe97f725e7cfc1
oznurtopcu <topcuoznur121@gmail.com>
2025-02-13 11:46:41 +0300

    add homePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './layout/Header'
-import PageContent from './layout/PageContent'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./layout/Header";
+import PageContent from "./layout/PageContent";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
   return (
     <PageContent>
-      <h1>Hello main world!</h1>
+      <HomePage />
     </PageContent>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="container">
+      <div>home page slider</div>
+      Hello home page world!
+    </div>
+  );
+}
