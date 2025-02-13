a6bdef24c21e3af46770eaba6035fde3bc579eee
oznurtopcu <topcuoznur121@gmail.com>
2025-02-14 01:46:40 +0300

    fix footer text

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,9 +3,9 @@ import { Facebook, Instagram, Twitter } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="font-bold font-montserrat">
-      <div className="flex flex-col m-20 gap-8">
-        <div className="flex flex-col gap-4">
-          <h3 className="text-[#252B42]">Get In Touch</h3>
+      <div className="flex flex-col m-20 gap-10">
+        <div className="flex flex-col gap-6">
+          <h3 className="text-[#252B42] text-2xl">Get In Touch</h3>
           <p className="text-[#737373] ">
             the quick fox jumps over the lazy dog
           </p>
@@ -16,26 +16,32 @@ export default function Footer() {
           </div>
         </div>
         <div>
-          <h3 className="text-[#252B42]">Company Info</h3>
-          <ul className="text-[#737373] leading-8">
-            <li>About Us</li>
-            <li>Carrier</li>
-            <li>We are hiring</li>
-            <li>Blog</li>
-          </ul>
-        </div>
-        <div>
-          <h3 className="text-[#252B42]">Features</h3>
-          <ul className="text-[#737373] leading-8">
-            <li>Business marketing</li>
-            <li>User Analytic</li>
-            <li>Live Chat</li>
-            <li>Unlimited Support</li>
-          </ul>
+          <div>
+            <h3 className="text-[#252B42] text-2xl">Company Info</h3>
+            <ul className="text-[#737373] leading-10 my-4">
+              <li>About Us</li>
+              <li>Carrier</li>
+              <li>We are hiring</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-[#252B42] text-2xl">Features</h3>
+            <ul className="text-[#737373] leading-10 my-4">
+              <li>Business marketing</li>
+              <li>User Analytic</li>
+              <li>Live Chat</li>
+              <li>Unlimited Support</li>
+            </ul>
+          </div>
         </div>
       </div>
+
       <div className="m-6 text-[#737373] text-center">
-        <p>Made With Love By Figmaland All Right Reserved</p>
+        <p>
+          Made With Love By <br />
+          Figmaland All Right Reserved
+        </p>
       </div>
     </footer>
   );
