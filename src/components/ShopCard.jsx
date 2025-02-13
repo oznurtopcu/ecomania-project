149e88b798cae00b8ca589c4da698ab9652cbf89
oznurtopcu <topcuoznur121@gmail.com>
2025-02-13 16:12:39 +0300

    Update ShopCard.jsx

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,16 +1,16 @@
 export default function ShopCard() {
   return (
-    <div className="max-w-sm rounded-md overflow-hidden shadow-lg">
+    <div className="max-w-sm overflow-hidden shadow-lg">
       <img
-        className="w-full"
-        src="https://placehold.co/324x500"
+        className="w-full "
+        src="https://placehold.co/324x500/orange/png"
         alt="shop-card"
       />
-      <div className="px-6 py-4 relative">
-        <div className="absolute bottom-0 left-0 px-6 pb-4">
-          <div className="bg-white text-center py-2 px-4 rounded-full font-bold text-sm">
+      <div className="relative">
+        <div className="absolute bottom-0 left-0 px-6 pb-6">
+          <button className="bg-white text-center py-3 px-8 font-bold text-sm">
             WOMEN
-          </div>
+          </button>
         </div>
       </div>
     </div>
