5a2fd75b90eb4f662b0c5d94b56447b210a23a20
oznurtopcu <topcuoznur121@gmail.com>
2025-02-13 13:13:02 +0300

    delete div to pageContent

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,14 +1,14 @@
 import Footer from "./Footer";
-import Header from "./Header"
+import Header from "./Header";
 
-export default function PageContent( {children} ) {
-    //Bu component header ve footer'ın her sayfada aynı şekilde görünmesini sağlıyor. 
-    //Componentleri PageContent ile sarmalıyoruz.
-    return(
-        <div>
-            <Header/>
-            <main> {children} </main>
-            <Footer/>
-        </div>
-    );
+export default function PageContent({ children }) {
+  //Bu component header ve footer'ın her sayfada aynı şekilde görünmesini sağlıyor.
+  //Componentleri PageContent ile sarmalıyoruz.
+  return (
+    <>
+      <Header />
+      <main> {children} </main>
+      <Footer />
+    </>
+  );
 }
