45d0dafd0f6b5db5f76fb7f626dddcb9b282cd9a
oznurtopcu <topcuoznur121@gmail.com>
2025-02-13 12:05:16 +0300

    add sample layout to homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
 export default function HomePage() {
   return (
     <div className="container">
-      <div>home page slider</div>
-      Hello home page world!
+      <div className="slider">
+        <img src="https://placehold.co/414x753" alt="slider" />
+      </div>
+      <div className="shopping-cards">
+        <div className="main-content">
+          <h3>EDITOR'S PICK</h3>
+          <p>Problems trying to resolve the conflict between </p>
+        </div>
+        <div className="main-content">
+          <img src="https://placehold.co/324x500" alt="image-1" />
+          <img src="https://placehold.co/324x500" alt="image-2" />
+        </div>
+        <div className="main-content">
+          <img src="https://placehold.co/324x242" alt="image-3" />
+          <img src="https://placehold.co/324x242" alt="image-4" />
+        </div>
+      </div>
     </div>
   );
 }
