f10850ffab83d3a30bebd053c084224825218db9
oznurtopcu <topcuoznur121@gmail.com>
2025-02-13 16:15:25 +0300

    add ShopCard component in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,15 +27,20 @@ export default function HomePage() {
         <Slider slides={sampleSlides} />
         {/* <img src="https://placehold.co/414x753" alt="slider" /> */}
       </div>
-      <div className="shopping-cards">
-        <div className="main-content">
-          <h3>EDITOR'S PICK</h3>
+      <div className="shopping-cards my-15">
+        <div className="main-content text-center m-15">
+          <h3 className="font-bold text-[#252B42]">EDITOR'S PICK</h3>
           <p>Problems trying to resolve the conflict between </p>
         </div>
-        <div className="main-content">
-          <ShopCard />
-          <img src="https://placehold.co/324x500" alt="image-1" />
-          <img src="https://placehold.co/324x500" alt="image-2" />
+        <div className="">
+          <div className="main-content flex flex-col items-center gap-6 mb-6">
+            <ShopCard />
+            <ShopCard />
+          </div>
+          <div className="main-content flex flex-col items-center gap-6">
+            <ShopCard />
+            <ShopCard />
+          </div>
         </div>
         <div className="main-content">
           <img src="https://placehold.co/324x242" alt="image-3" />
