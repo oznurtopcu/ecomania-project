2295f803915ec4bf6d7dd82ce21adb4ed29df13a
oznurtopcu <topcuoznur121@gmail.com>
2025-02-14 17:58:54 +0300

    edit shopCard style

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,17 +1,15 @@
-export default function ShopCard() {
+export default function ShopCard({ className }) {
   return (
-    <div className="max-w-sm overflow-hidden shadow-lg">
+    <div className={`relative w-full h-full shadow-lg ${className}`}>
       <img
-        className="w-full "
-        src="https://placehold.co/324x500/orange/png"
+        className="w-full h-full object-cover"
+        src="https://picsum.photos/200/300"
         alt="shop-card"
       />
-      <div className="relative">
-        <div className="absolute bottom-0 left-0 px-6 pb-6">
-          <button className="bg-white text-center py-3 px-8 font-bold text-sm">
-            WOMEN
-          </button>
-        </div>
+      <div className="absolute bottom-0 left-0 px-6 pb-6">
+        <button className="bg-white text-center py-3 px-8 font-bold text-sm">
+          WOMEN
+        </button>
       </div>
     </div>
   );
