40875af75675c128b269a8fcf0c8f5ebe0241c0e
oznurtopcu <topcuoznur121@gmail.com>
2025-02-16 23:37:31 +0300

    delete text

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -194,20 +194,6 @@ export default function ShopPage() {
   ];
 
   return (
-    <div className="">
-      {/* Centered content container */}
-      <div className="container mx-auto px-4">
-        <div className="product-cards my-20">
-          <div className="text-center mb-12 flex flex-col gap-6 items-center">
-            <p className="text-center text-sky-500 font-bold">
-              Practice Advice
-            </p>
-            <h2 className="font-bold text-[#252B42] text-4xl leading-12">
-              Featured Products
-            </h2>
-            <p className="text-[#737373] text-xl">
-              Problems trying to resolve the conflict between the two major
-            </p>
     <div className="container mx-auto px-4">
       <div className="page-breadcrumb flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-8 px-6 py-10 lg:mx-35">
         <h3 className="text-[#252B42] text-2xl font-bold">Shop</h3>
