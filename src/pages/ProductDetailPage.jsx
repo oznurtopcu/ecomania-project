7a7943b1a76ae72c67902898f55059d88dabdeb3
oznurtopcu <topcuoznur121@gmail.com>
2025-02-17 14:08:03 +0300

    add bestseller products

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -115,6 +115,20 @@ export default function ProductDetailPage() {
       <div className="product-detail">
         <ProductDetailCard />
       </div>
+      <div className="bestseller-cards px-4 lg:mx-35">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+          {products.map((product, index) => (
+            <div key={index} className="w-full flex justify-center my-8">
+              <button
+                onClick={() => history.push("/shop/product")}
+                className="cursor-pointer"
+              >
+                <ProductCard key={product.id} product={product} />
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
