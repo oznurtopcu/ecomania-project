54ceb851900284efc72ddc52d074321d380e3345
oznurtopcu <topcuoznur121@gmail.com>
2025-02-18 14:56:46 +0300

    create shoppingCartActions

---
+++ src/store/actions/shoppingCartActions.js
@@ -0,0 +1,20 @@
+// Action Types
+export const SET_CART = "SET_CART";
+export const SET_PAYMENT = "SET_PAYMENT";
+export const SET_ADDRESS = "SET_ADDRESS";
+
+// Action Creators
+export const setCart = (cart) => ({
+  type: SET_CART,
+  payload: cart,
+});
+
+export const setPayment = (payment) => ({
+  type: SET_PAYMENT,
+  payload: payment,
+});
+
+export const setAddress = (address) => ({
+  type: SET_ADDRESS,
+  payload: address,
+});
