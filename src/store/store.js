13f2522ba34940f31690aa2ca6e0c9057ed91c76
oznurtopcu <topcuoznur121@gmail.com>
2025-02-18 14:35:43 +0300

    add applyMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { legacy_createStore as createStore } from "redux";
 import reducers from "./reducers/index";
+import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 
-export const store = createStore(reducers);
+export const store = createStore(reducers, applyMiddleware(logger, thunk));
