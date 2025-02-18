0e6e22dff7cd40289500d99b94622af16dd07b09
oznurtopcu <topcuoznur121@gmail.com>
2025-02-18 14:54:47 +0300

    create productReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import clientReducer from "./clientReducer";
+import productReducer from "./productReducer";
 
 const reducers = combineReducers({
   client: clientReducer,
+  product: productReducer,
 });
 
 export default reducers;
