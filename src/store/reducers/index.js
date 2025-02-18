e21f1f0cf7d5c91b706b7813c8ae1c0d286ca61f
oznurtopcu <topcuoznur121@gmail.com>
2025-02-18 15:00:07 +0300

    create shoppingCartReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import clientReducer from "./clientReducer";
 import productReducer from "./productReducer";
+import shoppingCartReducer from "./shoppingCartReducer";
 
 const reducers = combineReducers({
   client: clientReducer,
   product: productReducer,
+  shoppingCart: shoppingCartReducer,
 });
 
 export default reducers;
