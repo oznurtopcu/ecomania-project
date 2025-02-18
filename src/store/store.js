20f9c8a316a97daa509028635e3fdee121e0e216
oznurtopcu <topcuoznur121@gmail.com>
2025-02-18 14:32:45 +0300

    provide app from store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
 import { legacy_createStore as createStore } from "redux";
 import reducers from "./reducers/index";
 
-export const myStore = createStore(reducers);
+export const store = createStore(reducers);
