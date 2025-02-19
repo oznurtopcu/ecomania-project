35a39c8bd7a0bc77adf18b6df2c68ce07d951cf5
oznurtopcu <topcuoznur121@gmail.com>
2025-02-19 10:10:23 +0300

    create role actions and role reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import clientReducer from "./clientReducer";
 import productReducer from "./productReducer";
 import shoppingCartReducer from "./shoppingCartReducer";
+import roleReducer from "./roleReducer";
 
 const reducers = combineReducers({
   client: clientReducer,
   product: productReducer,
   shoppingCart: shoppingCartReducer,
+  roles: roleReducer,
 });
 
 export default reducers;
