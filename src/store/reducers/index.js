ed1968c215e22eb5e633c09a2ab9e10b3157ee16
oznurtopcu <topcuoznur121@gmail.com>
2025-02-19 11:16:47 +0300

    add loginReducer to combinedReducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,12 +3,14 @@ import clientReducer from "./clientReducer";
 import productReducer from "./productReducer";
 import shoppingCartReducer from "./shoppingCartReducer";
 import roleReducer from "./roleReducer";
+import { loginReducer } from "./loginReducer";
 
 const reducers = combineReducers({
   client: clientReducer,
   product: productReducer,
   shoppingCart: shoppingCartReducer,
   roles: roleReducer,
+  login: loginReducer,
 });
 
 export default reducers;
