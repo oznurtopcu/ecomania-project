207b081ad2bfb750445fdf7bbd21bb0bf69ed4dd
oznurtopcu <topcuoznur121@gmail.com>
2025-02-19 11:16:55 +0300

    Create loginActions.js

---
+++ src/store/actions/loginActions.js
@@ -0,0 +1,45 @@
+import { api } from "../../api/axios";
+import { setUser } from "./clientActions";
+
+// Action Types
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+// Action Creators
+export const loginRequest = () => ({
+  type: LOGIN_REQUEST,
+});
+
+export const loginSuccess = (user) => ({
+  type: LOGIN_SUCCESS,
+  payload: user,
+});
+
+export const loginFailure = (error) => ({
+  type: LOGIN_FAILURE,
+  payload: error,
+});
+
+// Thunk Action
+export const loginUser = (credentials) => {
+  return async (dispatch) => {
+    dispatch(loginRequest());
+    try {
+      const response = await api.post("/login", credentials);
+
+      // User bilgisini client reducer'a gönderiyoruz
+      dispatch(setUser(response.data.user));
+
+      if (credentials.rememberMe) {
+        localStorage.setItem("token", response.data.token);
+      }
+
+      dispatch(loginSuccess(response.data.user));
+      return response.data;
+    } catch (error) {
+      dispatch(loginFailure(error.message));
+      throw error;
+    }
+  };
+};
