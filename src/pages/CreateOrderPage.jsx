b4a5a3f55f997ee039830c817ea6b47c98b5cb37
oznurtopcu <topcuoznur121@gmail.com>
2025-02-28 13:29:17 +0300

    create address reducer and actions

--- src/pages/CreateOrderPage.jsx
+++ src/pages/CreateOrderPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { api } from "../api/axios";
 import { toast } from "react-toastify";
 import cities from "../cities";
 import {
